7460429e0302da5e3bdabf3904ea28a8a54b2033
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-09-28 03:05:09 +0200

    :dizzy: add animations and transitions

---
+++ main.js
@@ -0,0 +1,54 @@
+gsap.to('#letter_s', {
+    duration: 1,
+    rotation: 360,
+    transformOrigin: "50% 50%",
+    ease: Linear.easeInOut,
+    yoyo: true,
+    repeat: -1,
+    repeatDelay: 2,
+});
+
+const tween = gsap.to("#myLogo", { duration: 0.1, x: 6, repeat: -1, yoyo: true, ease: "sine.inOut", paused: true });
+
+const tweenTwo = gsap.to("#menuHover", { duration: 0.1, x: 6, repeat: -1, yoyo: true, ease: "sine.inOut", paused: true });
+
+const tweenThree = gsap.to("#myLogoLarge", { duration: 0.1, x: 6, repeat: -1, yoyo: true, ease: "sine.inOut", paused: true });
+
+document.getElementById('myLogo').addEventListener('mouseover', () => {
+    tween.play();
+});
+document.getElementById('myLogo').addEventListener('mouseleave', () => {
+    tween.pause();
+});
+document.getElementById('menuHover').addEventListener('mouseover', () => {
+    tweenTwo.play();
+});
+document.getElementById('menuHover').addEventListener('mouseleave', () => {
+    tweenTwo.pause();
+});
+document.getElementById('myLogoLarge').addEventListener('mouseover', () => {
+    tweenThree.play();
+});
+document.getElementById('myLogoLarge').addEventListener('mouseleave', () => {
+    tweenThree.pause();
+});
+
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menuBtn');
+const wrapper = document.querySelector('.wrapper');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    menuBtn.classList.toggle('activeBtn');
+});
+
+const okBtn = document.querySelector('.okBtn');
+const canBtn = document.querySelector('.canBtn');
+const cookies = document.querySelector('.cookies');
+
+okBtn.addEventListener('click', () => {
+    cookies.style.display = 'none';
+});
+canBtn.addEventListener('click', () => {
+    cookies.style.display = 'none';
+});
